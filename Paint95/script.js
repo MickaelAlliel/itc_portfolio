45654df842407873df5ba445c89d761e0ca68a19
Mickael Alliel <mickael.alliel@gmail.com>
2017-12-11 14:58:32 +0200

    Redesign all page Can now drag to draw Fixed loading times Made overlap possible Added round pen tip Added tool size

--- Paint95/script.js
+++ Paint95/script.js
@@ -1,14 +1,17 @@
 function checkPenTool() {
-	var pens = $('[name="pens"]'); // 0 = pen, 1 = eraser
+	var pens = $('[name="pens"]'); // 0 = round pen, 1 = square pen, 2 = eraser
 
 	if (pens[0].checked) {
-		$(document.body).css('cursor', 'initial');
-		return 'pen';
+		$(document.body).css('cursor', 'initial'); // TODO: Change to a round pointer
+		return 'roundPen';
 	} else if (pens[1].checked) {
-		$(document.body).css('cursor', 'cell');
+		$(document.body).css('cursor', 'initial'); // TODO: Change to a square pointer
+		return 'squarePen';
+	} else if (pens[2].checked) {
+		$(document.body).css('cursor', 'cell'); 
 		return 'eraser';
 	} else {
-		return 'pen';
+		return 'roundPen';
 	}
 }
 
@@ -16,14 +19,25 @@ function getColor() {
 	return $('#penColor').val();
 }
 
-function colorTile(tile, penTool) {
-	if (tile) {
-		if (penTool == 'pen') {
-			tile.css('background-color', getColor());
-		} else if (penTool == 'eraser') {
-			tile.css('background-color', 'initial');
-		}
+function draw(canvas, penTool, size, locX, locY) {
+	var drawedDiv = $('<div/>').attr('class', 'drawedDiv');
+
+	drawedDiv.css('background-color', getColor())
+				.css('top', locY)
+				.css('left', locX)
+				.css('width', size)
+				.css('height', size);
+
+	if (penTool == 'roundPen') {
+		drawedDiv.addClass('round');
 	}
+	else if (penTool == 'squarePen') {
+		// Nothing to do
+	} else if (penTool == 'eraser') {
+		drawedDiv.css('background-color', 'initial');
+	}
+
+	canvas.append(drawedDiv);
 }
 
 function getCanvasWidth() {
@@ -34,57 +48,27 @@ function getCanvasHeight() {
 	return parseInt($('#canvasHeight').val());
 }
 
-function updateCanvasSize(canvas, tileSize) {
+function updateCanvasSize(canvas) {
 	canvas.css('width', getCanvasWidth());
 	canvas.css('height', getCanvasHeight());
-
-	generateGrid(canvas, tileSize);
 }
 
-function generateGrid(canvas, tileSize) {
-	// Resetting canvas
+function resetCanvas(canvas) {
 	canvas.html('');
-
-	// Generating tiles
-	for (var i = 0; i < getCanvasWidth(); i = i + tileSize) {
-		for (var j = 0; j < getCanvasHeight(); j = j + tileSize) {
-			var tileElem = $('<div/>').attr('id', 'tile-' + i + '-' + j).css('width', tileSize).css('height', tileSize).css('display', 'inline-block');
-			canvas.append(tileElem);
-		}
-	}
 }
-
-function resetCanvas(canvas, tileSize) {
-	generateGrid(canvas, tileSize);
-}
-
-function getTileFromLocation(locX, locY, tileSize) {
-	for (var i = 0; i < getCanvasWidth(); i = i + tileSize) {
-		for (var j = 0; j < getCanvasHeight(); j = j + tileSize) {
-			var tile = $('#tile-' + i + '-' + j);
-			
-			var tileLoc = tile.offset();
-
-			if ( (tileLoc.top < locY && tileLoc.top + tileSize > locY) && 
-				(tileLoc.left < locX && tileLoc.left + tileSize > locX) ) {
-				//console.log(tile.offset()); // Clicked tile
-				return tile;
-			}
-		}
-	}
-}
-
 
 
 function generateHtmlStructure() {
 	var body = $(document.body);
+	var globalContainer = $('<div/>').attr('class', 'global-container');
 
 	// Toolbar
+	var toolbarContainer = $('<div/>').attr('class', 'container');
 	var toolbar = $('<div/>').attr('id', 'toolbar');
 
 
 	// Toolbar Size
-	var toolbarSize = $('<div/>').attr('id', 'toolbarSize');
+	var toolbarSize = $('<div/>').attr('id', 'toolbarSize').attr('class', 'toolbar-section');
 
 	var labelWidth = $('<label/>').append('Width');
 	var labelHeight = $('<label/>').append('Height');
@@ -92,47 +76,81 @@ function generateHtmlStructure() {
 	var heightInput = $('<input/>').attr('type', 'text').attr('id', 'canvasHeight').attr('value', '500');
 
 	var updateBtn = $('<button/>').attr('id', 'updateCanvasSize').append('Update');
-	var resetBtn = $('<button/>').attr('id', 'resetCanvas').append('Reset to blank');
+	var resetBtn = $('<button/>').attr('id', 'resetCanvas').append('Clear Canvas');
+
+	var toolbarBtns = $('<div/>').attr('id', 'toolbarBtns');
+	
 
 	toolbarSize.append(labelWidth)
 				.append(widthInput)
+				.append('<br/>')
 				.append(labelHeight)
-				.append(heightInput)
-				.append(updateBtn)
+				.append(heightInput);
+
+	toolbarBtns.append(updateBtn)
+				.append('<br/>')
 				.append(resetBtn);
+				
 
 	// Toolbar Colors
-	var toolbarColors = $('<div/>').attr('id', 'toolbarColors');
+	var toolbarColors = $('<div/>').attr('id', 'toolbarColors').attr('class', 'toolbar-section');
 
-	var labelColor = $('<label/>').append('Color');
-	var colorInput = $('<input/>').attr('type', 'color').attr('id', 'penColor');
+	var colorInput = $('<input/>').attr('type', 'color').attr('id', 'penColor').css('width', '100px').css('height', '40px');
 
-	toolbarColors.append(labelColor)
-					.append(colorInput);
+	toolbarColors.append(colorInput);
 
 	// Toolbar Pens
-	var toolbarPens = $('<div/>').attr('id', 'toolbarPens');
+	var toolbarPens = $('<div/>').attr('id', 'toolbarPens').attr('class', 'toolbar-section');
 
-	var penTool = $('<input/>').attr('type', 'radio').attr('name', 'pens').attr('id', 'pen').attr('checked', true);
-	var penLabel = $('<label/>').append('Pen').append(penTool);
+	var roundPenIcon = $('<img/>').attr('src', 'round-pen.png').attr('class', 'toolIcon');
+	var squarePenIcon = $('<img/>').attr('src', 'square-pen.png').attr('class', 'toolIcon');
+	var eraserIcon = $('<img/>').attr('src', 'eraser.png').attr('class', 'toolIcon');
+
+	var roundPenTool = $('<input/>').attr('type', 'radio').attr('name', 'pens').attr('id', 'roundPen').attr('checked', true);
+	var roundPenLabel = $('<label/>').append(roundPenIcon).append(roundPenTool);
+	var squarePenTool = $('<input/>').attr('type', 'radio').attr('name', 'pens').attr('id', 'squarePen').attr('checked', true);
+	var squarePenLabel = $('<label/>').append(squarePenIcon).append(squarePenTool);
 	var eraserTool = $('<input/>').attr('type', 'radio').attr('name', 'pens').attr('id', 'eraser');
-	var eraserLabel = $('<label/>').append('Eraser').append(eraserTool);
+	var eraserLabel = $('<label/>').append(eraserIcon).append(eraserTool);
 
-	toolbarPens.append(penLabel).append(eraserLabel);
+	var toolSizeLabel = $('<label/>').append('Tool Size');
+	var toolSizeInput = $('<input/>').attr('type', 'range').attr('min', '2').attr('max', '50')
+									.attr('step', '1').attr('id', 'toolSize').attr('value', '5');
+	var toolSizeValueLabel = $('<label/>').attr('id', 'toolSizeValue').append(' 5');
+
+	toolbarPens.append(roundPenLabel)
+				.append('<br/>')
+				.append(squarePenLabel)
+				.append('<br/>')
+				.append(eraserLabel)
+				.append('<br/>')
+				.append('<br/>')
+				.append('<br/>')
+				.append(toolSizeLabel)
+				.append('<br/>')
+				.append(toolSizeInput)
+				.append(toolSizeValueLabel);
 
 	// ----------- TOOLBAR
 	toolbar.append(toolbarSize)
+			.append(toolbarBtns)
 			.append(toolbarColors)
 			.append(toolbarPens);
 
 
 	// Canvas
+	var canvasContainer = $('<div/>').attr('class', 'container canvas-container');
 	var canvas = $('<div/>').attr('id', 'canvas');
 
 
 	// -----------------------
-	body.append(toolbar);
-	body.append(canvas);
+	toolbarContainer.append(toolbar);
+	canvasContainer.append(canvas);
+
+	globalContainer.append(toolbarContainer);
+	globalContainer.append(canvasContainer);
+
+	body.append(globalContainer);
 }
 
 $(function() {
@@ -141,18 +159,37 @@ $(function() {
 
 	// Set global variables
 	var canvas = $('#canvas');
-	var tileSize = 10; // in pixels
+	var toolSize = 5;
 	var penTool = checkPenTool(); // Defaults as pen tool
+
+	var canDraw = false;
 
 	// Set Event Listeners
 	$('#updateCanvasSize').click(function() { updateCanvasSize(canvas, tileSize) });
 	$('#resetCanvas').click(function() { resetCanvas(canvas, tileSize) });
 	$('[name="pens"]').change(function() { penTool = checkPenTool() });
-	
+	$('#toolSize').change(function() {
+ 		toolSize = $('#toolSize').val();
+		$('#toolSizeValue').html(' ' + toolSize); 
+	});
+
+	$(document).mousemove(function(e) {
+		if ((e.pageX >= canvas[0].offsetLeft && e.pageX <= canvas[0].offsetLeft + canvas[0].offsetWidth - toolSize)
+			 && (e.pageY >= canvas[0].offsetTop && e.pageY <= canvas[0].offsetTop + canvas[0].offsetHeight - toolSize)) {
+			if (canDraw) {
+				draw(canvas, penTool, toolSize, e.pageX, e.pageY);
+			}
+		}
+	});
+
 	$(document).mousedown(function(e) {
-		colorTile(getTileFromLocation(e.pageX, e.pageY, tileSize), penTool);
+		canDraw = true;
+	});
+
+	$(document).mouseup(function(e) {
+		canDraw = false;
 	});
 
 	// Initialize Canvas
-	updateCanvasSize(canvas, tileSize);
+	updateCanvasSize(canvas, toolSize);
 });
